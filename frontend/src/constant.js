272ecb173ff3344ad07c94f8a14c08bae079494b
Jeeva <146157053+Jeevanandam5@users.noreply.github.com>
2025-06-15 13:51:29 +0530

    Update constant.js

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://e-commerce-furniture-backend-9vxf.onrender.com";
+export const BASE_URL = "http://localhost:5000";
 export const PRODUCT_URL = "/api/products";
 export const USER_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
