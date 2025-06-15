ef814654b75607feeba3ee70196acac90e841855
Jeeva <146157053+Jeevanandam5@users.noreply.github.com>
2025-06-15 14:24:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin:['http://localhost:5173', 'https://e-commercee-furnitures.onrender.com'],
   credentials: true,
 }));
 
